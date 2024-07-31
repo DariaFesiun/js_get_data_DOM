2597d4af6d7a9fb790e163982d59b0024c833eff
daradanilova <lemma1102@gmail.com>
2024-07-31 12:50:54 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,13 @@
+"use strict";
+const population = [
+    ...document.querySelectorAll(".population")
+];
+const popArr = population.map((item)=>item.textContent.split(",").join(""));
+const totalCount = popArr.reduce((acc, item)=>acc + +item, 0);
+const averageCount = totalCount / popArr.length;
+const totalPop = document.querySelector(".total-population");
+const averagePop = document.querySelector(".average-population");
+totalPop.textContent = totalCount.toLocaleString("en");
+averagePop.textContent = averageCount.toLocaleString("en");
+
+//# sourceMappingURL=index.f75de5e1.js.map
